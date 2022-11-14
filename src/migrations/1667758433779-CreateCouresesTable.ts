969c215171ce771a2fb2e8faa09bd613324bf4cd
egespindola <eds.gesp@gmail.com>
2022-11-14 16:07:21 -0300

    feat. docker

---
+++ src/migrations/1667758433779-CreateCouresesTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCouresesTable1667758433779 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
